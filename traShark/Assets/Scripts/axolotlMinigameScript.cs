e8ef7e9a8b50ec92230c1b770666e97ec82c8987
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2021-11-28 19:17:57 +0100

    Implement Axolotl

---
+++ traShark/Assets/Scripts/axolotlMinigameScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class axolotlMinigameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.childCount == 0)
+        {
+            transform.parent.GetComponent<StartMinigame>().Saving();
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //other.GetComponent<lineDraggable>().Destruction();
+    }
+}
---
+++ traShark/Assets/Scripts/bag.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class bag : MonoBehaviour
+{
+    // The plane the object is currently being dragged on
+    private Plane dragPlane;
+	private Vector2 boxSize = new Vector2(30f,30f);
+    // The difference between where the mouse is on the drag plane and 
+    // where the origin of the object is on the drag plane
+    private Vector3 offset;
+    private int rybka;
+    private Camera myMainCamera; 
+
+    void Start()
+    {
+        myMainCamera = Camera.main; // Camera.main is expensive ; cache it here
+        GetComponent<PolygonCollider2D>().isTrigger = true;
+    }
+
+    private void Update()
+    {
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position,boxSize, 0, Vector2.zero);
+
+		if(hits.Length > 0)
+		{
+            rybka = 0;
+
+			foreach(RaycastHit2D rc in hits)
+			{
+				if(rc.transform.GetComponent<axolotlMinigameScript>())
+				{
+                    rybka =+ 1;
+				}
+			}
+
+            if(rybka == 0)
+            {
+                Destroy(gameObject);
+            }
+		}
+    }
+
+    void OnMouseDown()
+    {
+        dragPlane = new Plane(myMainCamera.transform.forward, transform.position); 
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition); 
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist); 
+        offset = transform.position - camRay.GetPoint(planeDist);
+    }
+
+    void OnMouseDrag()
+    {   
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition); 
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        transform.position = camRay.GetPoint(planeDist) + offset;
+    }
+
+    public void Destruction()
+    {
+        Destroy(gameObject);
+    }
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
